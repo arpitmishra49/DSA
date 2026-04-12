7609885dfd7939cb134c5a0618c996081b478911
arpitmishra49 <arpitmishra4925@gmail.com>
2026-04-13 00:36:16 +0530

    revision

--- Strings/FrequencyOfCharacters.cpp
+++ Strings/FrequencyOfCharacters.cpp
@@ -19,7 +19,7 @@ int main() {
     // Apply formula
     for (int i = 0; i < 256; i++) {
         int k = freq[i];
-        result += (k * (k + 1)) / 2;
+        result += (k * (k + 1)) / 2;//yis=gf
     }
 
     cout << "Number of substrings: " << result << endl;
